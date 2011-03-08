18a66f806298f1d6f53753432726ff7f9e39569b
Dan Nawara <dnawara@groupon.com>
2011-03-07 20:57:00 -0600

    added simple menu manager

--- wp-content/themes/northdown/menu_template.php
+++ wp-content/themes/northdown/menu_template.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Lunch Menu
+<?php /* Template Name: Menus
  */
 /**
  * @package WordPress
@@ -7,32 +7,45 @@
 
 get_header(); ?>
 
+    <?php
+      $categories = get_terms('menu_categories');
+      $menu_type = the_title('','',false);
+    ?>
 		<div id="primary">
       <div id="content" role="main">
         <article>
           <h1><?php echo the_title(); ?></h1>
           <?php
-            $args = array( 
-              'tax_query' => array(
-                array(
-                  'taxonomy' => 'menu_types',
-                  'field' => 'name',
-                  'terms' => the_title('','',false)
+            foreach ($categories as $menu_category){
+              $args = array( 
+                'tax_query' => array(
+                  array(
+                    'taxonomy' => 'menu_types',
+                    'field' => 'name',
+                    'terms' => $menu_type
+                  ),
+                  array(
+                    'taxonomy' => 'menu_categories',
+                    'field' => 'slug',
+                    'terms' => $menu_category->slug
+                  )
                 )
-              )
-            );
-
-            $loop = new WP_Query( $args );
-
-            while ( $loop->have_posts() ) : $loop->the_post();
+              );
+              $loop = new WP_Query( $args );
+              if ($loop->post_count){
+                echo '<h2>';
+                  echo $menu_category->name;
+                echo '</h2>';
+              while ( $loop->have_posts() ) : $loop->the_post();?>
+                <h3><?php the_title(); ?></h3>
+                <?php
+                echo '<div class="entry-content">';
+                  the_content();
+                echo '</div>';
+              endwhile;
+              };
+            };
             ?>
-              <h2><?php the_title(); ?></h2>
-            <?php
-              echo '<div class="entry-content">';
-              the_content();
-              echo '</div>';
-            endwhile;
-          ?>
         </article>
 			</div><!-- #content -->
 		</div><!-- #primary -->
